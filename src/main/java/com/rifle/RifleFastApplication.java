d635fe8b9d3b12f0c77a992ae820ea626d167923
MaxCosmos <renyh2001@outlook.com>
2024-11-11 10:30:09 +0800

    初始化Rifle-fast项目

---
+++ src/main/java/com/rifle/RifleFastApplication.java
@@ -0,0 +1,18 @@
+package com.rifle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifleFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RifleFastApplication.class, args);
+        System.out.println("====================================================");
+        System.out.println("=                                                  =");
+        System.out.println("=             rifle-fast start success             =");
+        System.out.println("=                                                  =");
+        System.out.println("====================================================");
+    }
+
+}
---
+++ src/test/java/com/rifle/RifleFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RifleFastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
